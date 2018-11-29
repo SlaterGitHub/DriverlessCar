04ea61fce217c180aa92732aa64482b049a253c1
RYSL <32707042+SlaterGitHub@users.noreply.github.com>
2018-11-29 10:58:57 +0000

    main is up to date

--- RaspberryPi/Main.py
+++ RaspberryPi/Main.py
@@ -2,30 +2,15 @@ from threading import Thread
 import Connection as cn
 import Camera as cam
 import time
-#import DistanceSensor as dist
+import DistanceSensor as dist
 import DriveCar as dc
 c, r = cn.setConnection()
-
-direction = 3
-speed = 0.2
-
 """time.sleep(1)
 r = cn.getConnection()"""
-drive = Thread(target = dc.setDirection)
-
-drive.start()
-
+#direction, speed = Thread(target = cn.recvData).start()
 while True:
     frame = cam.getFrame()
-    w, h = frame.shape[:2]
-    #print(str(w), "x", str(h))
     cn.sendData(frame, c)
     direction, speed = cn.recvData()
-    print("-----------------------------------------------------")
     print(direction)
     print(speed)
-    print("_________________________________________________________")
-    dc.setVariables(direction, speed)
-    #distance = dist.getDistance()
-
-drive.join()
