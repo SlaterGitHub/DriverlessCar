b9e8bd6bda92d09a8fef6f632af057e757d7fcee
RYSL <32707042+SlaterGitHub@users.noreply.github.com>
2018-11-29 10:55:00 +0000

    Added detection and main is up to date

--- RaspberryPi/Main.py
+++ RaspberryPi/Main.py
@@ -2,17 +2,30 @@ from threading import Thread
 import Connection as cn
 import Camera as cam
 import time
-import DistanceSensor as dist
+#import DistanceSensor as dist
 import DriveCar as dc
 c, r = cn.setConnection()
+
+direction = 3
+speed = 0.2
+
 """time.sleep(1)
 r = cn.getConnection()"""
-direction, speed = Thread(target = cn.recvData).start()
+drive = Thread(target = dc.setDirection)
+
+drive.start()
+
 while True:
     frame = cam.getFrame()
+    w, h = frame.shape[:2]
+    #print(str(w), "x", str(h))
     cn.sendData(frame, c)
     direction, speed = cn.recvData()
+    print("-----------------------------------------------------")
     print(direction)
     print(speed)
-    distance = dist.getDistance()
-    dc.setDirection(direction, speed)
+    print("_________________________________________________________")
+    dc.setVariables(direction, speed)
+    #distance = dist.getDistance()
+
+drive.join()
